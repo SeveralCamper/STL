5642b3b22244a12516d80433390d18ac0fc73354
alkhimovich_m <mikha.alkhimovich@mail.ru>
2024-06-21 03:53:53 +0700

    main: добавил коммит с разбором queue, дописал stack

---
+++ Queue/run.sh
@@ -0,0 +1,2 @@
+g++ Queue.cpp -o Queue
+./Queue
--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -35,8 +35,29 @@ int main()
 	stack_1.push(4);
 	stack_1.emplace(5); // emplace работает быстрее push, т.к. создает не новый объект в коллекцию, а заменят уже имеющийся.
 
-    std::cout << "Stack top: " << stack_1.top() << std::endl; // Выведет 5
+    std::cout << "Stack top: " << stack_1.top() << std::endl << std::endl; // Выведет 5
     stack_1.pop();
+
+	// Чтобы использовать stack как адаптер для другого контейнера, нужно передать тип контейнра вторым параметром
+
+	std::stack<int, std::deque<int>> stack_2;
+
+    stack_2.push(1);
+    stack_2.push(2);
+    stack_2.push(3);
+
+    std::cout << "Stack top: " << stack_2.top() << std::endl; // Выведет 3
+    stack_2.pop(); // Удаляем верхний элемент
+
+    stack_2.push(4);
+
+    std::cout << "Stack size: " << stack_2.size() << std::endl; // Выведет 3
+    std::cout << "Stack: ";
+    while (!stack_2.empty()) {
+        std::cout << stack_2.top() << " "; // Выведет 4 2 1 (обратный порядок добавления)
+        stack_2.pop();
+    }
+    std::cout << std::endl;
 	
 	return 0;
 }
